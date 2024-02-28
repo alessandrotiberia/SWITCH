a79e91dcc0b250e268e5f389cfff6d51e596779a
alessandrotiberia <150442623+alessandrotiberia@users.noreply.github.com>
2024-02-28 16:19:12 +0100

    first commit

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main{
+    public static void main(String[] args) {
+        
+        String nome= "Alessandro";
+
+        switch (nome) {
+            case "Luca":
+                System.out.println("ciao Luca");
+                break;
+        
+            case "alessio":
+            System.out.println("ciao alessio");
+            break;
+
+            default:
+            System.out.println("ciao ad un altra persona con un diverso nome");
+                break;
+
+        }
+    }
+
+}
